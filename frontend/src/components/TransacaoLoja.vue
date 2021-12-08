11870a462a87eff6789ba87d96c9ec4418d5fac7
Kodigilo <gabriel@kodigilo.com>
2021-12-07 23:00:57 -0300

    Tela inicial do front end

---
+++ frontend/src/components/TransacaoLoja.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <table>
+            <tr>
+                <th>Cartão</th>
+                <th>CPF</th>
+                <th>Valor</th>
+            </tr>
+            <tr v-for="transacao in transacoes" :key="transacao.id">
+                <td>{{ transacao.cartao }}</td>
+                <td>{{ transacao.cpf }}</td>
+                <td>{{ transacao.valor | currency }}</td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Transacao",
+    props : ['transacoes']
+}
+</script>
+
+<style scoped>
+
+</style>
